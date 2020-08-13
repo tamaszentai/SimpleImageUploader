a2ac28365c636f43f4b949cb1c1d2c1e4a1711b5
Tamas Zentai <zentaitamas@gmail.com>
2020-08-13 18:17:36 +0100

    I always forget to do this...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [file, setFile] = useState();
+
+  const onFileSelected = (event) => {
+    if(event.target.files[0]){
+      setFile(event.target.files[0]);
+    }
+  }
+
+   // Uploading image to Cloud Storage
+   const imageUploadHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (file !== '') {
+        // Creating a FormData object
+        let fileData = new FormData();
+
+        // Adding the 'image' field and the selected file as value to our FormData object
+        // Changing file name to make it unique and avoid potential later overrides
+        fileData.set(
+          'image',
+          file
+          // `${Date.now()}-${file.name}`
+        );
+
+        await axios({
+          method: 'post',
+          url: "http://localhost:5000/api/upload",
+          data: fileData,
+          headers: { 'Content-Type': 'multipart/form-data' },
+        });
+      }
+    } catch (error) {
+     
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple image uploader to Firebase</h1>
+      <form onSubmit={imageUploadHandler}>
+        <input type="file" onChange={onFileSelected} />
+        <button>Upload</button>
+      </form>
     </div>
   );
 }
